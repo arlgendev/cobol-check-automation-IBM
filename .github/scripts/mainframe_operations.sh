29c02ad3e752bce100e7e05e62c7eae0c73a4729
Alessandro Lucena <arl.gendev@gmail.com>
2026-07-07 22:41:05 -0300

    Rename ZOWE_USERNAME to Z_UN in script

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -10,10 +10,10 @@ export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
 # Check Java availability
 java -version
 
-# Set ZOWE_USERNAME
-ZOWE_USERNAME="Z74881"  # Replace with your actual username
+# Set Z_UN
+Z_UN="Z74881"  # Replace with your actual username
 
-echo "ZOWE_USERNAME -> $ZOWE_USERNAME"
+echo "Z_UN -> $Z_UN"
 
 # Change to the cobolcheck directory
 cd cobolcheck
@@ -49,20 +49,20 @@ run_cobolcheck() {
   # Keep it as is in the code
 
   echo "Checking variable after this point: "
-  FULLCBL="//'$ZOWE_USERNAME.CBL($program)'"
-  FULLJCL="//'$ZOWE_USERNAME.JCL($program)'"
-  echo "ZOWE_USERNAME -> $ZOWE_USERNAME"
-  echo "program       -> $program"
-  echo "FULLCBL       -> $FULLCBL"
-  echo "FULLJCL       -> $FULLJCL"
+  FULLCBL="//'$Z_UN.CBL($program)'"
+  FULLJCL="//'$Z_UN.JCL($program)'"
+  echo "Z_UN    -> $Z_UN"
+  echo "program -> $program"
+  echo "FULLCBL -> $FULLCBL"
+  echo "FULLJCL -> $FULLJCL"
   
   # Check if CC##99.CBL was created, regardless of cobolcheck exit status
   if [ -f "testruns/CC##99.CBL" ]; then
     # Copy to the MVS dataset
-    if cp testruns/CC##99.CBL "//'${ZOWE_USERNAME}.CBL($program)'"; then
-      echo "Copied CC##99.CBL to $ZOWE_USERNAME.CBL($program)"
+    if cp testruns/CC##99.CBL "//'${Z_UN}.CBL($program)'"; then
+      echo "Copied CC##99.CBL to $Z_UN.CBL($program)"
     else
-      echo "Failed to copy CC##99.CBL to $ZOWE_USERNAME.CBL($program)"
+      echo "Failed to copy CC##99.CBL to $Z_UN.CBL($program)"
     fi
   else
     echo "CC##99.CBL not found for $program"
@@ -70,13 +70,13 @@ run_cobolcheck() {
 
   # Copy the JCL file if it exists
   if [ -f "${program}.JCL" ]; then
-    if cp ${program}.JCL "//'$ZOWE_USERNAME.JCL($program)'"; then
-      echo "Copied ${program}.JCL to $ZOWE_USERNAME.JCL($program)"
+    if cp ${program}.JCL "//'$Z_UN.JCL($program)'"; then
+      echo "Copied ${program}.JCL to $Z_UN.JCL($program)"
       # Submit job to run testing version of the program
       submit ${program}.JCL
       echo "Submitted job ${program}.JCL"
     else
-      echo "Failed to copy ${program}.JCL to $ZOWE_USERNAME.JCL($program)"
+      echo "Failed to copy ${program}.JCL to $Z_UN.JCL($program)"
     fi
   else
     echo "${program}.JCL not found"
